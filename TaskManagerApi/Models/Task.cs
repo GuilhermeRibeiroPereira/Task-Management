908619200c46066b4e3e57421f60b28e1ef44b78
Guilherme Pereira <pereira.gui887@gmail.com>
2025-09-28 15:57:04 +0100

    feat: add more invariants to TaskItem (#1)

--- TaskManagerApi/Models/Task.cs
+++ TaskManagerApi/Models/Task.cs
@@ -3,9 +3,12 @@ namespace TaskManagerApi.Models;
 
 public class TaskItem {
     public int Id { get; set; }
-    [Required]
+    [Required(ErrorMessage="Please enter a Title")]
+    [StringLength(100, ErrorMessage = "Title length can't be more than 100")]
+    [MinLength(2, ErrorMessage = "Title length must be at least 2")]
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public string Status { get; set; } = string.Empty; // "Todo", "InProgress", "Done"
-    public int Priority { get; set; }
+    public string Status { get; set; } = "Todo"; // "Todo", "InProgress", "Done"
+    [Range(0, int.MaxValue, ErrorMessage = "Priority must be greater or equal to 0")]
+    public int Priority { get; set; } = 0; // higher number = higher priority
 }
